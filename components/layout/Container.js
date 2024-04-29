5c67dc6c35c6ce39f291cc2214bbd4bce9181a04
Felipe Chaves <felipecchaves@gmail.com>
2024-04-29 05:18:40 +0000

    Imagem

--- components/layout/Container.js
+++ components/layout/Container.js
@@ -1,63 +1,14 @@
-import { useLayoutEffect, useEffect } from 'react'
-import { setLogged } from '../../redux/actions/user'
-import { setAlert } from '../../redux/actions/main'
-import { getCookie, hasCookie } from 'cookies-next'
-import { connect } from 'react-redux'
-import { useRouter } from 'next/router'
-
-import Aside from './Aside'
-import Footer from './Footer'
-import Alerta from './Alerta'
-
 import styles from '../../styles/components/layout/Container.module.sass'
 
-const Container = ({ children, logged, setLogged, setAlert }) => {
-  const router = useRouter()
-
-  useEffect(() => {
-    setAlert({
-      tipo: 'alerta',
-      titulo: 'Site em construção!',
-      mensagem: 'Todas as funções estão em teste.'
-    })
-  }, [setAlert])
-
-  useEffect(() => {
-    const pattern = new RegExp('gestor')
-    const url = router.asPath
-    if (!logged && pattern.test(url)) {
-      router.push('/login')
-    }
-  }, [logged])
-
-  useLayoutEffect(() => {
-    if (hasCookie('logged') && !logged) {
-      const user = getCookie('logged')
-      if (user === 'true') {
-        setLogged(true)
-      }
-    }
-  }, [])
+const Container = ({ children }) => {
 
   return (
     <>
       <main className={styles.container}>
-        {logged && <Aside />}
         {children}
       </main>
-      <Footer />
-      <Alerta />
     </>
   )
 }
 
-const mapState = (state) => ({
-  logged: state.user.logged
-})
-
-const mapDispatch = {
-  setLogged,
-  setAlert
-}
-
-export default connect(mapState, mapDispatch)(Container)
+export default Container
